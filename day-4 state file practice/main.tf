a527b2c87ff06f961a0413ecc838001daa81eb10
root <root@ip-172-31-21-134.eu-central-1.compute.internal>
2025-07-11 08:43:29 +0000

    modifying the force delete in main.tf

--- day-4 state file practice/main.tf
+++ day-4 state file practice/main.tf
@@ -4,7 +4,9 @@ provider "aws" {
 
 
 resource "aws_instance" "dev1" {
-    instance_type = t2.micro
-    ami = ami-0229b8f55e5178b65
+  instance_type = "t2.micro"
+    ami = "ami-0229b8f55e5178b65"
     count = 2
 }
+
+#force_destroy = true  # Deletes even if the bucket has files or objects
