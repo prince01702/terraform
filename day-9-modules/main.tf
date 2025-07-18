158e83c7884623e2544af281491dd7c2f4b3f6e3
NEGI Chandresh <chandresh.negi@soprasteria.com>
2025-07-18 13:10:52 +0530

    making vauce right

--- day-9-modules/main.tf
+++ day-9-modules/main.tf
@@ -3,7 +3,7 @@ module "prdo" {
     source = "../templet_for_module-day-9"
     ami_id = "ami-05ffe3c48a9991133"
     instance_type = "t2.micro"
-    key = "Hello.key"
-    az = "eu-central-1"
+    key = "hello.key"
+    az = "eu-central-1a"
 
 }
