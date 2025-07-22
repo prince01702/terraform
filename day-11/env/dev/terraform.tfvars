16610353016e0f6ce51c026db1f4ae94d4ac1431
NEGI Chandresh <chandresh.negi@soprasteria.com>
2025-07-22 15:13:35 +0530

    adding dev env

---
+++ day-11/env/dev/terraform.tfvars
@@ -0,0 +1,7 @@
+cidr_block = "10.0.0.0/16"
+public_subnet_cidr_block = "10.0.1.0/24"
+env = "dev"
+instance_type = "t2.micro"
+ami_id = "ami-05ffe3c48a9991133"
+avability_zone = "eu-central-1a"
+
---
+++ day-11/env/dev/variable.tf
@@ -0,0 +1,7 @@
+variable "cidr_block" {}
+variable "public_subnet_cidr_block" {}
+variable "env" {}
+variable "ami_id" {}
+variable "instance_type" {}
+variable "avaibility_zone" {}
+variable "" {}
